08372bf63c59e476326660a181597ce5964259e0
marcosCanedo <mckndo@gmail.com>
2019-09-27 00:21:16 +0200

    commitVentanaInicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VentanadetalleComponent } from './modulos/ventanadetalle/ventanadetalle.component';
+import { CochesComponent } from './modulos/coches/coches.component';
+import { MotosComponent } from './modulos/motos/motos.component';
+import { ModulosModule } from './modulos/modulos.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: VentanadetalleComponent },
+  { path: 'coches', component: CochesComponent },
+  { path: 'motos', component: MotosComponent }
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), ModulosModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
