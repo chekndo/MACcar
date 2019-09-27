4a27e6cd20fdf04d2ab9a8207bad40d8eb03e529
marcos <mckndo>
2019-09-27 10:34:05 +0200

    commitDListaCocheDinamica

--- src/app/modulos/coches/coches.component.ts
+++ src/app/modulos/coches/coches.component.ts
@@ -7,7 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CochesComponent implements OnInit {
 
-  constructor() { }
+  coches: any[];
+  constructor() {
+    const coches1={
+      marca: 'Mustang',
+      modelo: 'GT-500',
+      año: '1968',
+      cilindrada: '5800cc'
+    }
+    const coches2={
+      marca: 'Nissan',
+      modelo: 'Skyline',
+      año: '1999',
+      cilindrada: '2600cc'
+    }
+    const coches3={
+      marca: 'Audi',
+      modelo: 'RS5',
+      año: '2017',
+      cilindrada: '2900'
+    }
+    this.coches = [];
+    this.coches.push(coches1);
+    this.coches.push(coches2);
+    this.coches.push(coches3);
+
+   }
 
   ngOnInit() {
   }
